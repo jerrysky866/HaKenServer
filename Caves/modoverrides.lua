8f5759adb227b88b840a018853df78956f063302
jerrysky866 <41415188+jerrysky866@users.noreply.github.com>
2020-10-25 02:40:01 +0800

    Initial commit

---
+++ Caves/modoverrides.lua
@@ -0,0 +1,30 @@
+return {
+  ["workshop-2208128427"]={
+    configuration_options={
+      LOOT_PUMP_EQUIP=true,
+      LOOT_PUMP_SCALE=1,
+      LOOT_PUMP_SOUND=true,
+      LOOT_PUMP_SPEED=1,
+      LOOT_PUMP_TWOZN=false 
+    },
+    enabled=true 
+  },
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  } 
+}
---
+++ Run_Server.bat
@@ -0,0 +1,3 @@
+cd /D "D:\Steam\steamapps\common\Don't Starve Together\bin"
+start "Master" dontstarve_dedicated_server_nullrenderer -console -cluster HaKenServer -shard Master
+start "Caves" dontstarve_dedicated_server_nullrenderer -console -cluster HaKenServer -shard Caves
